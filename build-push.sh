ce4ea2d3702bf18898dede76f1d5e0a8ec7433bd
Redwid <redwid@gmail.com>
2019-04-18 23:17:56 +0100

    Update docker login config

--- build-push.sh
+++ build-push.sh
@@ -24,7 +24,7 @@ then
     echo
 else
     echo '[INFO] Login to docker'
-    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
 fi
 
 echo '[INFO] Build and push image'
